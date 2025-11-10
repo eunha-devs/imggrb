59cd2d224e2241630a3f2ff38c51542b18c314a8
eunha-devs <h4n4.business@gmail.com>
2025-11-10 22:43:27 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
 const app = express();
-app.use("/images", express.static("images"));
+app.use(express.static("."));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
