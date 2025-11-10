14e45f49ae5d9dc3f906fd7c56981f81c44c0cce
eunha-devs <h4n4.business@gmail.com>
2025-11-10 22:40:20 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+import express from "express";
+const app = express();
+app.use("/images", express.static("images"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
